7cb4a84b431792a358880cdd919895c9b75d557c
root <hassenjasim3@gmail.com>
2022-11-02 19:42:53 +0300

    update

--- src/lace/io.rs
+++ src/lace/io.rs
@@ -7,7 +7,8 @@ pub struct Source {
 impl Source {
     pub fn new<S>(name: &S) -> Self
     where S: AsRef<std::ffi::OsStr> + ?Sized + std::fmt::Display {
-        let buffer = std::fs::read_to_string(name.to_string());
+        let file_path = std::path::Path::new(name);
+        let buffer = std::fs::read_to_string(&file_path);
         let content = match buffer {
             Ok(content) => content,
             Err(message) => panic!("{}", message),
